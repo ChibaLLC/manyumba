cb8e9f6493dc402ac4e1ddbce07e05af5bdcf855
kgarchie <archiethebig@gmail.com>
2025-02-08 20:25:31 +0300

    shad

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	compatibilityDate: "2024-11-01",
+	devtools: { enabled: true },
+	modules: ["reka-ui/nuxt", "shadcn-nuxt"],
+	future: {
+		compatibilityVersion: 4,
+	},
+	extends: ["github:kgarchie/nuxt-starter#1"],
+	nitro: {
+		imports: {
+			dirs: ["./shared/utils", "./shared/types"],
+		},
+	},
+	imports: {
+		dirs: ["../shared/types", "../shared/utils", "./shad-cn/ui"],
+	},
+	shadcn: {
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: "./app/shad-cn",
+	},
+});
