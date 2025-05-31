2006c58c9e2c5716b331964dc308d523f39ac92f
PrimalDjin <archiethebig@gmail.com>
2025-05-31 20:11:25 +0300

    updated nuxt

--- app/components/Listing/Location.vue
+++ app/components/Listing/Location.vue
@@ -1,20 +1,24 @@
+<script setup lang="ts">
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+</script>
 <template>
-	<ListingContainer class="flex flex-col">
-		<div>
-			<h1>Location</h1>
-			<p>
-				Drop a pin to the precise location of the property. You can use your current location if you are on site
-				(recommended).
-			</p>
-			<p>You can use the search to get the general area. Then reposition the pin.</p>
-		</div>
-		<div class="flex flex-col">
-			<Button>Use Current Location</Button>
-			<Input>
-				<Icon name="local:location" />
-			</Input>
-		</div>
-		<Map />
-		<Button class="self-end"></Button>
-	</ListingContainer>
+  <ListingContainer class="flex flex-col">
+    <div>
+      <h1>Location</h1>
+      <p>
+        Drop a pin to the precise location of the property. You can use your current location if you are on site
+        (recommended).
+      </p>
+      <p>You can use the search to get the general area. Then reposition the pin.</p>
+    </div>
+    <div class="flex flex-col">
+      <Button>Use Current Location</Button>
+      <Input>
+        <Icon name="local:location" />
+      </Input>
+    </div>
+    <Map />
+    <Button class="self-end"></Button>
+  </ListingContainer>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,50 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2024-11-01",
-	devtools: { enabled: true },
-	modules: ["reka-ui/nuxt", "shadcn-nuxt", "@nuxt/fonts", "@nuxt/icon"],
-	future: {
-		compatibilityVersion: 4,
-	},
-	extends: ["github:kgarchie/nuxt-starter#1"],
-	nitro: {
-		imports: {
-			dirs: ["./shared/utils", "./shared/types"],
-		},
-	},
-	imports: {
-		dirs: ["../shared/types", "../shared/utils"],
-	},
-	shadcn: {
-		/**
-		 * Directory that the component lives in.
-		 * @default "./components/ui"
-		 */
-		componentDir: "./app/components/ui",
-	},
-	icon: {
-		size: "24px",
-		customCollections: [
-			{
-				dir: "./app/assets/icons",
-				prefix: "local",
-			},
-		],
-		class: "icon",
-		mode: "svg",
-	},
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxtjs/i18n"],
+  future: {
+    compatibilityVersion: 4,
+  },
+  components: {
+    dirs: [
+      {
+        ignore: ["ui"],
+      },
+    ],
+  },
+  nitro: {
+    experimental: {
+      websocket: true,
+    },
+    imports: {
+      dirs: ["./shared/utils", "./shared/types"],
+    },
+  },
+  imports: {
+    dirs: ["../shared/types", "../shared/utils"],
+  },
+  icon: {
+    size: "24px",
+    customCollections: [
+      {
+        dir: "./app/assets/icons",
+        prefix: "local",
+      },
+    ],
+    class: "icon",
+    mode: "svg",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  i18n: {
+    defaultLocale: "en",
+    bundle: {
+      optimizeTranslationDirective: false,
+    },
+    locales: [{ code: "en", name: "English", file: "en.json" }],
+    langDir: "../app/locales",
+  },
 });
